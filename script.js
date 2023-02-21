ac8efce14d936713fefb608a306ec2a8ba046b93
abdullah1606 <96784351+abdullah1606@users.noreply.github.com>
2023-02-21 21:35:24 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC3GEG8dSA2XbAq48JhIrrg6qPndfOAP2Q",
+  authDomain: "newjs2023.firebaseapp.com",
+  projectId: "newjs2023",
+  storageBucket: "newjs2023.appspot.com",
+  messagingSenderId: "71088445785",
+  appId: "1:71088445785:web:be0e67752f5d9069199448",
+};
+
+const app = initializeApp(firebaseConfig);
